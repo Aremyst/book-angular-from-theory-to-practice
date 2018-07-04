2b48a87ccba6d2c57b3b009dfa3773fb2b722767
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-04 06:53:07 +0300

    Change templates, so that: - AppComponent adds 1 Joke to JokeListComponent via ng-content. - JokeListComponent renders 2 Jokes by itself. - Both AppComponent and JokeListComponent use ng-content for JokeComponent.

--- Page144_AngularCLIJokeComponentProject/Joke/src/app/joke-list/joke-list.component.ts
+++ Page144_AngularCLIJokeComponentProject/Joke/src/app/joke-list/joke-list.component.ts
@@ -8,7 +8,10 @@ import {Joke} from "../joke";
   styleUrls: ['./joke-list.component.css']
 })
 export class JokeListComponent implements OnInit {
-  jokes: Joke[] = [];
+  jokes: Joke[] = [
+    new Joke("What did the cheese say when it looked in the mirror", "Hello-me (Halloumi)"),
+    new Joke("What kind of cheese do you use to disguise a small horse", "Mask-a-pony (Mascarpone)")
+  ];
 
   addJoke() {
     this.jokes.unshift(new Joke("What did the cheese say when it looked in the mirror", "Hello-me (Halloumi)"));
