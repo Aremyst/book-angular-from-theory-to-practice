b3534a7e76d7c3ee9b44d91be7928827b04c13cd
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-04 07:11:06 +0300

    Use @ViewChild decorator to get the instance of the *first*(there are 2) JokeComponent inside the view of JokeListComponent. This instance has meaningful data inside "ngAfterViewInit" handler.

--- Page144_AngularCLIJokeComponentProject/Joke/src/app/joke-list/joke-list.component.ts
+++ Page144_AngularCLIJokeComponentProject/Joke/src/app/joke-list/joke-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Joke} from "../joke";
+import {JokeComponent} from "../joke/joke.component";
 
 
 @Component({
@@ -7,11 +8,22 @@ import {Joke} from "../joke";
   templateUrl: './joke-list.component.html',
   styleUrls: ['./joke-list.component.css']
 })
-export class JokeListComponent implements OnInit {
+export class JokeListComponent implements OnInit,
+  AfterViewInit {
   jokes: Joke[] = [
     new Joke("What did the cheese say when it looked in the mirror", "Hello-me (Halloumi)"),
     new Joke("What kind of cheese do you use to disguise a small horse", "Mask-a-pony (Mascarpone)")
   ];
+
+  @ViewChild(JokeComponent) jokeViewChild: JokeComponent;
+
+  constructor() {
+    console.log(`new - jokeViewChild is ${this.jokeViewChild}`);
+  }
+
+  ngAfterViewInit() {
+    console.log(`ngAfterViewInit - jokeViewChild is ${this.jokeViewChild}`);
+  }
 
   addJoke() {
     this.jokes.unshift(new Joke("What did the cheese say when it looked in the mirror", "Hello-me (Halloumi)"));
