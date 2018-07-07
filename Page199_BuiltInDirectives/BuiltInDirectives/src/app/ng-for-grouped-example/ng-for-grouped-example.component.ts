ecd40e43a99e8c81999cc9922fd142d2508f6481
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-07 07:04:05 +0300

    Add nested *ngFor loop

---
+++ Page199_BuiltInDirectives/BuiltInDirectives/src/app/ng-for-grouped-example/ng-for-grouped-example.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-for-grouped-example',
+  templateUrl: './ng-for-grouped-example.component.html',
+  styleUrls: ['./ng-for-grouped-example.component.css']
+})
+export class NgForGroupedExampleComponent {
+  peopleByCountry: any[] = [
+    {
+      'country': 'UK',
+      'people': [
+        {
+          "name": "Douglas Pace"
+        },
+        {
+          "name": "Mcleod Mueller"
+        },
+      ]
+    },
+    {
+      'country': 'US',
+      'people': [
+        {
+          "name": "Day Meyers"
+        },
+        {
+          "name": "Aguirre Ellis"
+        },
+        {
+          "name": "Cook Tyson"
+        }
+      ]
+    }
+  ];
+}
