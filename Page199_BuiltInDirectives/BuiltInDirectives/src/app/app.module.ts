48b4ef8112104045b4e82f77d0cb2d368f9a73a3
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-07 07:11:53 +0300

    Add NgIf Example

--- Page199_BuiltInDirectives/BuiltInDirectives/src/app/app.module.ts
+++ Page199_BuiltInDirectives/BuiltInDirectives/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgForExampleComponent } from './ng-for-example/ng-for-example.component';
 import { NgForGroupedExampleComponent } from './ng-for-grouped-example/ng-for-grouped-example.component';
+import { NgIfExampleComponent } from './ng-if-example/ng-if-example.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgForExampleComponent,
-    NgForGroupedExampleComponent
+    NgForGroupedExampleComponent,
+    NgIfExampleComponent
   ],
   imports: [
     BrowserModule
