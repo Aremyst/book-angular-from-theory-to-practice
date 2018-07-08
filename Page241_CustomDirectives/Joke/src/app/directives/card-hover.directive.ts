57dd442d52f5d867b9ad1be4e82cdae2049d2c52
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-08 21:24:14 +0300

    Add @HostBinding, so that Directive can change input property on Host element. Directive changes internal "isHovering" property, which in turn changes Host's [class.border-primary] input property when Host element is hovered.

--- Page241_CustomDirectives/Joke/src/app/directives/card-hover.directive.ts
+++ Page241_CustomDirectives/Joke/src/app/directives/card-hover.directive.ts
@@ -1,9 +1,11 @@
-import {Directive, ElementRef, HostListener, Renderer} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Renderer} from '@angular/core';
 
 @Directive({
   selector: '[ccCardHover]'
 })
 export class CardHoverDirective {
+
+  @HostBinding('class.border-primary') private isHovering: boolean;
 
   constructor(private el: ElementRef,
               private renderer: Renderer)
@@ -14,10 +16,14 @@ export class CardHoverDirective {
   @HostListener('mouseover') onMouseOver() {
     let part = this.el.nativeElement.querySelector('.card-text');
     this.renderer.setElementStyle(part, 'display', 'block');
+
+    this.isHovering = true;
   }
 
   @HostListener('mouseout') onMouseOut() {
     let part = this.el.nativeElement.querySelector('.card-text');
-    this.renderer.setElementStyle(part, 'display', 'none')
+    this.renderer.setElementStyle(part, 'display', 'none');
+
+    this.isHovering = false;
   }
 }
