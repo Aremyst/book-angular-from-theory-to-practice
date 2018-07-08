095f7bda23729d0ddb0f8f425def807744c5b031
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-08 07:32:59 +0300

    Use *ngFor, *ngIf, ngStyle to complete "homework" Article application.

--- Page239_BuiltInDirectives_ArticleComponent/Articles/src/app/app.module.ts
+++ Page239_BuiltInDirectives_ArticleComponent/Articles/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecentArticlesComponent } from './recent-articles/recent-articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecentArticlesComponent
   ],
   imports: [
     BrowserModule
