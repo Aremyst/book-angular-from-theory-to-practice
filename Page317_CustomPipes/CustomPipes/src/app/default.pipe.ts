b6bcb5536e242decffb8127428b61165f74639a8
Dmitriy Naimark <dmitrynaimark@gmail.com>
2018-07-13 22:17:51 +0300

    Add additional parameter to "DefaultPipe", use it on template. If true, it will replace "http" with "https" inside image src path.

--- Page317_CustomPipes/CustomPipes/src/app/default.pipe.ts
+++ Page317_CustomPipes/CustomPipes/src/app/default.pipe.ts
@@ -4,13 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'default'
 })
 export class DefaultPipe {
-  transform(value: string, fallback: string): string {
+  transform(value: string, fallback: string, forceHttps: boolean = false): string {
     let image = "";
     if (value) {
       image = value;
     } else {
       image = fallback;
     }
+
+    if (forceHttps) {
+      if (image.indexOf('https') === -1) {
+        image = image.replace("http", "https");
+      }
+    }
     return image;
+
   }
 }
